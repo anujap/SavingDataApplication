7483168e098cd45da27af7ac073a2b13c2bb26c7
anujap <anujapatki87@gmail.com>
2017-02-06 21:45:57 -0600

    Initial commit

---
+++ app/src/main/java/com/example/savingdataapplication/SavingDataMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.savingdataapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavingDataMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saving_data_main);
+    }
+}
